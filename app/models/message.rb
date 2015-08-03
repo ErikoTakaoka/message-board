76b124eaf09f98d1ca79116c047f1530ede5ae84
ErikoTakaoka <taka.hachi1123@gmail.com>
2015-08-03 13:54:00 +0000

    renew

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,8 @@
 class Message < ActiveRecord::Base
     # 名前は必須入力かつ20文字以内
     validates :name , length: {  maximum: 20 } , presence: true
-    #年齢は必須入力かつ0歳以上かつ114歳以下かつ数字以外入力出来ない
-    validates :age , numericality: true, length: {  minimum: 0, maximum: 114 } , presence: true
+    #年齢は必須入力かつ数字以外入力出来ない
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
